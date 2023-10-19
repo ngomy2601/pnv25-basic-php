6d9c967e5d41550a280dc2e5acd01c1f2bb35ab7
Tieu My <my.tieu@pn.com.vn>
2023-10-19 10:59:19 +0700

    practices-s2

---
+++ chapter_1/practices/s2/activity4.php
@@ -0,0 +1,24 @@
+<?php
+// A for loop to display a text without 'echo'
+for ($i = 0; $i < 3; $i++) {
+?>
+    hello world
+<?php
+}
+?>
+
+<?php
+// Other syntax to do the same
+for ($i = 0; $i < 3; $i++) :
+?>
+    hello world
+<?php
+endfor
+?>
+
+<?php
+// Instructions: Do the same to display 5 times your name
+
+
+
+?>
