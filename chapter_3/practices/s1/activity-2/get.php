5ac0daa425d4e2d02479b5c0d35275b4143aeff9
Tieu My <my.tieu@pn.com.vn>
2023-11-06 15:42:43 +0700

    practice-s1

---
+++ chapter_3/practices/s1/activity-2/get.php
@@ -0,0 +1,15 @@
+<?php
+require_once('templates/header.php');
+
+echo "<ul>";
+
+// TODO
+// - Your name is XXXXX
+// - Your hobbies areXXXXX
+// - You are a boy/girl
+
+
+echo "</ul>";
+
+
+require_once('templates/footer.php');
