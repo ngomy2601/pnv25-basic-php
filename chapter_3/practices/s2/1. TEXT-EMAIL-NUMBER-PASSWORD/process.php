713bd932eef6c0986df9b2b26dffe13c3704307e
Tieu My <my.tieu@pn.com.vn>
2023-11-06 16:13:36 +0700

    practice-s2

---
+++ chapter_3/practices/s2/1. TEXT-EMAIL-NUMBER-PASSWORD/process.php
@@ -0,0 +1,13 @@
+<?php // HEADER ?>
+<ul class="list-group">
+<?php
+   // YOUR CODE HERE
+?>
+    <li class="list-group-item"><?php // name; ?></li>
+    <li class="list-group-item"><?php // email; ?></li>
+    <li class="list-group-item"><?php // password; ?></li>
+    <li class="list-group-item"><?php // number; ?></li>
+    <li class="list-group-item"><?php // message; ?></li>
+
+</ul>
+<?php // FOOTER ?>
